3edeff5a814ad67f9f66761bad2257cee14b9feb
abrahimcse <abrahimcse@gmail.com>
2023-11-07 22:03:10 +0600

    project initiation

---
+++ Spring-Boot-DTO/src/test/java/com/abrahim/Spring/Boot/DTO/SpringBootDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrahim.Spring.Boot.DTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
